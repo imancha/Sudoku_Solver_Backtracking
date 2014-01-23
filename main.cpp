c4af9ab9e53c58b8f00585df9d1557a586033c37
Mohammad Abdul Iman Syah <imancha_266@ymail.com>
2014-01-24 05:07:08 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,5 @@
 #include <iostream>
 
-<<<<<<< HEAD
-/*	Start of Searching Sudoku Solutions	*/
-//	cek baris kolom yang kosong
-bool isEmpty(int grid[][9], int &row, int &col){
-	for(row=0; row<9; row++){
-		for(col=0; col<9; col++){
-			if(grid[row][col] == 0){
-				return true;
-				break;
-			}
-		}
-	}
-}
-
-//	cek nomor pada baris
-bool usedInRow(int grid[][9], int row, int num){
-	for(int col=0; col<9; col++){
-		if(grid[row][col] == num){
-=======
 /*	Start of Check Sudoku Questions	*/
 //	cek nomor pada baris yang sudah diisi
 bool checkRow(int grid[][9], int row, int col, int num){
@@ -31,12 +12,6 @@ bool checkRow(int grid[][9], int row, int col, int num){
 	}
 }
 
-<<<<<<< HEAD
-//	cek nomor pada kolom
-bool usedInCol(int grid[][9], int col, int num){
-	for(int row=0; row<9; row++){
-		if(grid[row][col] == num){
-=======
 //	cek nomor pada kolom yang sudah diisi
 bool checkCol(int grid[][9], int row, int col, int num){
 	for(int baris=row+1; baris<9; baris++){
@@ -48,13 +23,6 @@ bool checkCol(int grid[][9], int row, int col, int num){
 	}
 }
 
-<<<<<<< HEAD
-//	cek nomor pada box
-bool usedInBox(int grid[][9], int boxRow, int boxCol, int num){
-	for(int row=0; row<3; row++){
-		for(int col=0; col<3; col++){
-			if(grid[row+boxRow][col+boxCol] == num){
-=======
 //	cek nomor pada box yang sudah diisi
 bool checkBox(int grid[][9], int boxRow, int boxCol, int row, int col, int num){
 	for(int baris=0; baris<3; baris++){
@@ -68,15 +36,6 @@ bool checkBox(int grid[][9], int boxRow, int boxCol, int row, int col, int num){
 	}
 }
 
-<<<<<<< HEAD
-//	return true bila nomor belum digunakan pada baris & kolom & box
-bool isSafe(int grid[][9], int row, int col, int num){
-	return 	!usedInRow(grid,row,num) &&
-			!usedInCol(grid,col,num) &&
-			!usedInBox(grid,row-row%3,col-col%3,num);
-}
-
-=======
 //	return true bila nomor telah digunakan pada baris / kolom / box
 bool isSame(int grid[][9], int row, int col, int num){
 	return 	checkRow(grid,row,col,num) ||
